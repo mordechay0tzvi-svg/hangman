b53022c4c4c7bb84ac8273355b8962e7fcf6083d
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-05-13 15:25:16 +0300

    functions that adds the letter to the word if guessed right

--- functions.py
+++ functions.py
@@ -4,5 +4,17 @@ import random
 def pick_randon_word():
     return words[random.randint(0,len(words)-1)]
 
+def is_letter(char):
+    return len(char) == 1 and char.isalpha()
 
+def was_guessed(letter,guessed):
+    return letter in guessed
 
+def check(letter,word_to_guess):
+    return letter in word_to_guess
+
+def fill_word(word_guessed, word, letter):
+    for place in range(len(word)):
+        if word[place] == letter:
+            word_guessed[place] = letter
+    return word_guessed
