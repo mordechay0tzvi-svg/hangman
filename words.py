f2971898805b89f1b27647b98ac8f3fba36be0f5
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-05-13 15:10:28 +0300

    adding the words

---
+++ words.py
@@ -0,0 +1,10 @@
+words = [
+    "perform", "paralyzed", "arms", "fetch", "scratch itches", 
+    "plenty", "comb", "hair", "independent", "forced",
+    "graduating", "immense", "amusing", "contributors", "approximately", 
+    "companion", "pair", "capable", "runs it", "phrases",
+    "nursing", "despair", "helplessness", "dormitories", "raising", 
+    "laser beam", "respond", "institutions", "applications", "significance",
+    "spent", "organization", "owners", "task", "idea", 
+    "college", "program", "human", "accident", "objects"
+]
