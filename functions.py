5cf2dd14a83d9ae2e760b57818ef65c261569515
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-05-14 09:59:08 +0300

    even more functions

--- functions.py
+++ functions.py
@@ -19,6 +19,18 @@ def fill_word(word_guessed, word, letter):
             word_guessed[place] = letter
     return word_guessed
 
+def win_announcement(tries, word_shown):
+    print(f"Congrats! You guessed it in {10 - tries} guesses.\n")
+    print('===========================')
+    print("The word was:", "".join(word_shown))
+    print('===========================')
+
+def round_start(word_shown, guessed_wrong, tries):
+    print("Word:", " ".join(word_shown))
+    print('===========================')
+    print("Wrong guesses:", " ".join(guessed_wrong))
+    print(f"Tries left: {tries}")
+
 def main():
     print("🎮 Welcome to Hangman!")
     print("Guess the hidden word.\n")
@@ -26,14 +38,10 @@ def main():
     word = pick_randon_word()
     word_shown = ["_" for _ in range(len(word))]
     guessed_wrong = []
-    # guessed_right = []
     while tries > 0:
         guessed = guessed_wrong + word_shown
-        print("Word:", " ".join(word_shown))
-        print('===========================')
-        print("Wrong guesses:", " ".join(guessed_wrong))
-        print(f"Tries left: {tries}")
-        print('\n\n')
+        round_start(word_shown, guessed_wrong, tries)
+        print('\n')
         current = input("Guess a letter: ").lower()
         if not is_letter(current):
             print("Sorry, that is not a letter\n")
@@ -45,10 +53,7 @@ def main():
             print(f"✅ '{current}' is in the word!\n")
             word_shown = fill_word(word_shown, word, current)
             if "_" not in word_shown:
-                print(f"Congrats! You guessed it in {10 - tries} guesses.\n")
-                print('===========================')
-                print("The word was:", "".join(word_shown))
-                print('===========================')
+                win_announcement(tries, word_shown)
                 break
         else:
             print(f"❌ '{current}' is not in the word.\n")
